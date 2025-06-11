f3ed91b93695a17531a5a405284b4006645809ad
Victot <victorfarian@gmail.com>
2025-06-11 00:50:45 -0300

    Initialize repo

---
+++ liter_alura/src/main/java/com/victot_exe/liter_alura/LiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.victot_exe.liter_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+}
